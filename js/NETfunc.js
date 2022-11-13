0f2dba93a075f85d169a94c82d3abe038eeb8ae4
great123yay <great123yay@gmail.com>
2022-11-13 15:00:23 -0500

    packages installed

---
+++ js/NETfunc.js
@@ -0,0 +1,20 @@
+import * as THREE from "node_modules/three"
+import NET from "node_modules/vanta/dist/vanta.net.min.js"
+
+const body = document.getElementByClassName("fluid-container")
+
+VANTA.NET({
+  el: body,
+  THREE,
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x3fff77,
+  points: 20.00,
+  maxDistance: 27.00,
+  spacing: 17.00
+})
